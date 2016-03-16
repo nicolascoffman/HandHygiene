9b29bdd5c13891b3308d0eed7077ccee7b0d0472
Nicolas Coffman <nic@nicolascoffman.com>
2016-03-16 18:19:05 -0400

    added ObservationController

---
+++ app/Http/Controllers/ObservationController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ObservationController extends Controller
+{
+    //Shows page with most recent observation data and option to display specific observation
+    public function index($id){
+
+      //Load view for table with recent observations
+
+        //recent.blade.php
+
+        //Text bar to
+
+      //Use $id variable to link immediately to observation
+
+        //observation.blade.php
+
+    }
+}
