6843f237e317a5d2dd365b4fa16ec94d1a32a41c
Nicolas Coffman <nic@nicolascoffman.com>
2016-03-16 17:49:36 -0400

    added Observation migration file/schema

---
+++ database/migrations/2016_03_16_212223_Observation.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Observation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Store info for Obeservation
+        Schema::create('questions', function (Blueprint $table) {
+          $table->increments('ID');
+          $table->timestamps();
+          $table->integer('IP_ID');
+          $table->integer('Loc_ID');
+          $table->integer('Job_ID');
+          $table->integer('Moment_ID');
+          $table->integer('Result_ID');
+          $table->integer('Reason_ID');
+          $table->boolean('Gloves');
+          $table->binary('Entry Exit');
+          });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
