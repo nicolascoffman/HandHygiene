c26aba8fe375cf48fc1b6c0766e032074af0ef9e
Nicolas Coffman <nic@nicolascoffman.com>
2016-04-18 00:55:12 -0400

    migrations please

---
+++ app/Http/Controllers/Observation.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Observation;
+
+class Observation extends Controller
+{
+    //Shows form
+    public function index(){
+
+      return view('observe', ['responses' => ""]);
+
+    }
+
+    //Handles Submission
+
+    public function gotWhatever(Request $request){
+      $obj = new Observation;
+      $obj->Moment_ID  =  $request->input('Moment_ID');
+      $obj->Loc_ID =  $request->input('Loc_ID');
+      $obj->Job_ID =  $request->input('Job_ID');
+      $obj->save();
+      $questions = Observation::all();
+      return view('observe', ['responses' => $questions]);
+    }
+
+
+
+}
---
+++ database/migrations/2016_04_18_043101_HealthcareProfessional.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class HealthcareProfessional extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+
+        Schema::create('healthcareprofessionals', function (Blueprint $table) {
+          $table->increments('Job_ID');
+          $table->string('PositionName');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
