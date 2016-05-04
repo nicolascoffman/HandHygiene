2305bc88845aed4286c5de8276e493f26c063ed3
Nicolas Coffman <nic@nicolascoffman.com>
2016-05-04 18:02:49 -0400

    with logout

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -3,4 +3,5 @@
   <a href="/observe"  class="btn {{ (Request::is('observe') ? 'btn-default' :  'btn-primary') }}">Observe</a>
 <!--  <a href="/analyze"  class="btn {{ (Request::is('analyze') ? 'btn-default' :  'btn-primary') }}">Analyze</a> -->
   <a href="/edit"  class="btn {{ (Request::is('edit') ? 'btn-default' :  'btn-primary') }}">Edit</a>
+  <a href="/logout"  class="btn {{ (Request::is('edit') ? 'btn-default' :  'btn-primary') }}">Logout</a>
 </div>
